b3087dfac351f2f138c721178c25336ec3b203c7
Alexander Artemenko <svetlyak.40wt@gmail.com>
2012-09-24 13:14:15 +0400

    Now this plugins is able to work when nose-progressive plugin enabled.

--- nosenotifytmux/plugin.py
+++ nosenotifytmux/plugin.py
@@ -5,13 +5,12 @@ nosenotifytmux.plugin
 
 Nose plugin implementation.
 
-:copyright: 2011, Alexander Artemenko <svetlyak.40wt@gmail.com>
+:copyright: 2011-2012, Alexander Artemenko <svetlyak.40wt@gmail.com>
 :license: BSD, see doc/LICENSE for more details.
 """
 
 import os
 import subprocess
-from functools import partial
 from nose.plugins import Plugin
 
 
@@ -66,33 +65,7 @@ class NotifyPlugin(Plugin):
                 shell=True
             )
 
-    def prepareTestLoader(self, loader):
-        """Insert ourselves into loader calls to count tests.
-
-        The top-level loader call often returns lazy results, like a LazySuite.
-        This is a problem, as we would destroy the suite by iterating over it
-        to count the tests. Consequently, we monkeypatch the top-level loader
-        call to do the load twice: once for the actual test running and again
-        to yield something we can iterate over to do the count.
-
-        Stolen from nose-progressive.
-        """
-        def capture_suite(orig_method, *args, **kwargs):
-            """Intercept calls to the loader before they get lazy.
-
-            Re-execute them to grab a copy of the possibly lazy suite, and
-            count the tests therein.
-
-            """
-            self._total_tests += orig_method(*args, **kwargs).countTestCases()
-            return orig_method(*args, **kwargs)
-
-        # TODO: If there's ever a practical need, also patch loader.suiteClass
-        # or even TestProgram.createTests. createTests seems to be main top-
-        # level caller of loader methods, and nose.core.collector() (which
-        # isn't even called in nose) is an alternate one.
-        if hasattr(loader, 'loadTestsFromNames'):
-            loader.loadTestsFromNames = partial(capture_suite,
-                                                loader.loadTestsFromNames)
-        return loader
+    def prepareTest(self, test):
+        self._total_tests += test.countTestCases()
+        return test
 
