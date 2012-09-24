fe2e8cdabcfd93b3d148dfb514c02e6ab2639b03
Alexander Artemenko <svetlyak.40wt@gmail.com>
2012-09-24 13:40:36 +0400

    Change tab's color back in case when testing was interrupted by user.

--- nosenotifytmux/plugin.py
+++ nosenotifytmux/plugin.py
@@ -11,6 +11,8 @@ Nose plugin implementation.
 
 import os
 import subprocess
+import atexit
+
 from nose.plugins import Plugin
 
 
@@ -19,22 +21,35 @@ class NotifyPlugin(Plugin):
     name = 'tmux'
     _total_tests = 0
     _progress = 0
+    _finalized = False
 
     def begin(self):
         """Optionaly displays the start message."""
         if self.pane is not None:
             subprocess.call('tmux rename-window -t {pane} testing'.format(pane=self.pane), shell=True)
-            subprocess.call('tmux set-option -t {pane} window-status-bg {color} | grep blah'.format(pane=self.pane, color=self.progress_color), shell=True)
+            subprocess.call('tmux set-option -t {pane} window-status-bg {color} | grep ignore'.format(pane=self.pane, color=self.progress_color), shell=True)
+
+        atexit.register(self.finalize)
 
     def finalize(self, result=None):
         """Display success or failure message."""
+
+        if self._finalized:
+            return
+
         if self.pane is not None:
-            if result.wasSuccessful():
-                subprocess.call('tmux rename-window -t {pane} done'.format(pane=self.pane), shell=True)
-                subprocess.call('tmux set-option -t {pane} -u window-status-bg | grep blah'.format(pane=self.pane), shell=True)
+            if result is None:
+                subprocess.call('tmux rename-window -t {pane} interrupted'.format(pane=self.pane), shell=True)
+                subprocess.call('tmux set-option -t {pane} -u window-status-bg | grep ignore'.format(pane=self.pane), shell=True)
             else:
-                subprocess.call('tmux rename-window -t {pane} failed'.format(pane=self.pane), shell=True)
-                subprocess.call('tmux set-option -t {pane} window-status-bg {color} | grep blah'.format(pane=self.pane, color=self.error_color), shell=True)
+                if result.wasSuccessful():
+                    subprocess.call('tmux rename-window -t {pane} done > /dev/null'.format(pane=self.pane), shell=True)
+                    subprocess.call('tmux set-option -t {pane} -u window-status-bg | grep ignore'.format(pane=self.pane), shell=True)
+                else:
+                    subprocess.call('tmux rename-window -t {pane} failed'.format(pane=self.pane), shell=True)
+                    subprocess.call('tmux set-option -t {pane} window-status-bg {color} | grep ignore'.format(pane=self.pane, color=self.error_color), shell=True)
+
+        self._finalized = True
 
     def configure(self, options, conf):
         super(NotifyPlugin, self).configure(options, conf)
